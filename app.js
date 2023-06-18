3a8200c39d3fb5f6b577b270573f273907c4be10
Nenjuna <nenjunane@gmail.com>
2023-06-18 22:24:16 +0530

    second commit with port

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(8000, () => {
+app.listen(process.end.PORT, () => {
   console.log("Server running on port 8000");
 });
